9c9e3c19f2255453edc45809a33c67ced79987cb
TiaN1um <157285243+TiaN1um@users.noreply.github.com>
2026-01-07 14:15:42 +0100

    Update fixed.lua

--- fixed.lua
+++ fixed.lua
@@ -1,7 +1,8 @@
 -------------------------------------------------
 -- RAYFIELD LOAD
 -------------------------------------------------
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/TiaN1um/TianHub/main/fixed.lua"))()
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+
 
 
 -------------------------------------------------
